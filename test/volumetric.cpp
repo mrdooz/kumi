64555ecd932db285929f8ab10feb4d024a4b0e19
Magnus Osterlind <magnus.osterlind@gmail.com>
2012-05-04 08:58:41 +0200

    Tihi, I just had to hack the hardcoded animated camera :)

--- test/volumetric.cpp
+++ test/volumetric.cpp
@@ -57,12 +57,25 @@ bool VolumetricEffect::init() {
   return true;
 }
 
+static XMFLOAT4X4 mtx_at_time(const vector<KeyFrame> &frames, double time) {
+  for (int i = 0; i < (int)frames.size() - 1; ++i) {
+    if (time >= frames[i+0].time && time < frames[i+1].time) {
+      return frames[i].mtx;
+    }
+  }
+  return frames.back().mtx;
+}
+
 bool VolumetricEffect::update(int64 global_time, int64 local_time, int64 frequency, int32 num_ticks, 
                               float ticks_fraction) {
   if (_scene && !_scene->cameras.empty()) {
     Camera *camera = _scene->cameras[0];
+
+    XMFLOAT4X4 mtx = mtx_at_time(_scene->animation[camera->name], global_time / 1000.0);
+
     XMMATRIX lookat = XMMatrixTranspose(XMMatrixLookAtLH(
-      XMVectorSet(camera->pos.x,camera->pos.y,camera->pos.z,0),
+//      XMVectorSet(camera->pos.x,camera->pos.y,camera->pos.z,0),
+      XMVectorSet(mtx._41,mtx._42,mtx._43,0),
       XMVectorSet(camera->target.x,camera->target.y,camera->target.z,0),
       XMVectorSet(camera->up.x,camera->up.y,camera->up.z,0)));
 
