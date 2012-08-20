9cf24855651c1212ddce0aed0e0cb1e9e80c5813
Magnus Osterlind <magnus.osterlind@gmail.com>
2012-08-20 12:18:13 +0200

    mend

--- effects/particles.hlsl
+++ effects/particles.hlsl
@@ -47,10 +47,12 @@ void gs_main(point GsInput input[1], inout TriangleStream<PsInput> outputStream)
   float f = input[0].scale.y;
   float r = input[0].scale.x;
   
+  float3 camToParticle = pos - cameraPos.xyz;
+  
   // We're in camera space right now, so our up and right vectors
   // correspond to the cardinal axis. Doh :)
-  float left = pos - r;
-  float right = pos + r;
+  float left = pos.x - r;
+  float right = pos.x + r;
   float top = pos.y + r;
   float bottom = pos.y - r;
   float z = pos.z;
